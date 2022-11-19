8596032574799ad7772a422bf8cfd51193c88003
Sviatoslav <ecrprague.asi@gmail.com>
2022-11-19 19:04:55 +0300

    Task47 Done

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -9,3 +9,48 @@
 // 1 -3,3 8 -9,9
 
 // 8 7,8 -7,1 9
+
+Console.WriteLine("Введите число строк: ");
+bool isParsedLines = int.TryParse(Console.ReadLine(), out int lines);
+Console.WriteLine("Введите число столбцов: ");
+bool isParsedColumns = int.TryParse(Console.ReadLine(), out int columns);
+
+if (!isParsedLines || !isParsedColumns)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+
+PrintMatrix(FillMatrix(lines, columns));
+
+
+
+double[,] FillMatrix(int line, int column)
+{
+    double[,] matrix = new double[line, column];
+    Random random = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(-100, 110) / 10.0;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[   ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}   ");
+        }
+        Console.Write("]");
+        Console.WriteLine();
+    }
+}
+
